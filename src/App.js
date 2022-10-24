8ee4b5491fa56b7db9acd739d1039ef4abac1a08
ahmedisti <mdistiaqah@gmail.com>
2022-10-24 16:52:21 +0600

    button,typography,button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button , Typography} from '@mui/material';
+
+import { Add , Settings } from '@mui/icons-material';
+import styled from '@emotion/styled';
 
 function App() {
+
+  const BlueButton = styled(Button)({ //blue button component
+    backgroundColor :'skyblue',
+    color:'#888',
+    margin:5,
+    '&:hover':{
+      backgroundColor:'lightblue'
+    },
+    '&disabled':{
+      backgroundColor:'gray',
+      color:'white'
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Button variant="text">Text</Button>
+<Button startIcon={<Settings />} variant="contained" color="secondary" size="small">Setting</Button>
+<Button startIcon={<Add />} variant="contained" color="success" size="small">Add</Button>
+<Button variant="outlined" disabled>Outlined</Button>
+<Typography variant="h1" component="p">
+  I am p but style is H1
+</Typography>;
+<BlueButton>Blue</BlueButton>
+<BlueButton>red</BlueButton>
     </div>
   );
 }
