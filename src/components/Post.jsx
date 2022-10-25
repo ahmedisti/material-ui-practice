d9d462144ee3cc253f0f3ca8efa6cfe762eb09a6
ahmedisti <mdistiaqah@gmail.com>
2022-10-25 17:46:24 +0600

    Post component create to use it in the Feed section

---
+++ src/components/Post.jsx
@@ -0,0 +1,45 @@
+import { Favorite, FavoriteBorder, MoreVert, Share } from '@mui/icons-material'
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, Checkbox, IconButton, Typography } from '@mui/material'
+import React from 'react'
+
+const Post = () => {
+  return (
+    <Card>
+    <CardHeader
+      avatar={
+        <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+          R
+        </Avatar>
+      }
+      action={
+        <IconButton aria-label="settings">
+          <MoreVert />
+        </IconButton>
+      }
+      title="Material UI"
+      subheader="October 25, 2022"
+    />
+    <CardMedia
+      component="img"
+      height="20%"
+      image="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      alt="Paella dish"
+    />
+    <CardContent>
+      <Typography variant="body2" color="text.secondary">
+      Material UI is an open-source React component library that implements Google's Material Design. It includes a comprehensive collection of prebuilt components that are ready for use in production right out of the box.
+      </Typography>
+    </CardContent>
+    <CardActions disableSpacing>
+      <IconButton aria-label="add to favorites">
+      <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{color:'red'}}/>} />
+      </IconButton>
+      <IconButton aria-label="share">
+        <Share />
+      </IconButton>
+    </CardActions>
+  </Card>
+  )
+}
+
+export default Post
